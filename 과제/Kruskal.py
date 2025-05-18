dba10f0e6d4ad99d4c85a088ae4440daae92ee08
choijiuk1017 <86818323+choijiuk1017@users.noreply.github.com>
2025-05-18 19:05:56 +0900

    크루스칼 알고리즘 수정

--- 과제/Kruskal.py
+++ 과제/Kruskal.py
@@ -4,6 +4,8 @@ import heapq
 
 V = [ i+1 for i in range(8) ]
 
+# 간선의 정보
+# 연결된 간선 + 가중치
 E = { 
      (1, 2) : random.randint(1, 10), 
      (1, 3) : random.randint(1, 10), 
@@ -16,46 +18,69 @@ E = {
      (6, 7) : random.randint(1, 10),
      (6, 8) : random.randint(1, 10),
      (7, 8) : random.randint(1, 10)
-       } 
+    } 
 
 AdjA = [ [] for _ in range(len(V))]
 for row, col in E:
     AdjA[row-1].append(col)
     AdjA[col-1].append(row)
 
-def Kruskal(E : dict, AdjA : list):
-    visited = []
+
+# 크루스칼 알고리즘
+# 1. 모든 간선을 가중치 순으로 오름차순으로 정렬함
+# 2. 하나씩 꺼내면서 간선의 사이클이 생기지 않게 저장
+# 3. 위 과정을 거치며 간선이 모든 정점을 연결할 때까지 반복
+# 간선의 정보를 받아서 처리
+def Kruskal(E : dict):
+    visited = [] # 방문한 정점의 순서를 저장할 리스트
+    edgeList = [] # 연결할 간선을 저장할 리스트
+
+    # 간선의 가중치 기준으로 오름차순 정렬
     sortEdge = dict(sorted(E.items(), key= lambda item: item[1]))
+
+    # 정렬된 딕셔너리를 리스트로 저장
     vertexList = list(sortEdge.keys())
 
-    print(sortEdge)
-    print(vertexList)
-    first, edge = vertexList[0]
-    
-    visited.append(first)
-    visited.append(edge)
+    print(f"가중치 기준 정렬 : {sortEdge}")
+    print(f"정렬된 간선 리스트 : {vertexList}")
 
-    vertexList.pop(0)
+    # 간선 리스트가 빌 때까지 반복
     while(vertexList):
+
+        # 첫 번째 간선의 정점들을 받아옴
         first, edge = vertexList[0]
-        if(first in visited and edge not in visited):
+        
+        # 간선의 이웃 정점이 방문한 적이 없다면
+        if(edge not in visited):
+
+            # 해당 정점 방문 처리
             visited.append(edge)
+
+            # 해당 간선 저장
+            edgeList.append((first, edge))
+
+            # 리스트에서 삭제하고 넘어감
             vertexList.pop(0)
             continue
-        elif(first not in visited and edge in visited):
-            visited.append(first)
-            vertexList.pop(0)
-            continue
-        elif(first in visited and edge in visited):
+        
+        # 간선의 시작 정점과 이웃 정점이 이미 방문한 적이 있는 경우, 즉 이미 연결되어 있는 경우
+        if(first in visited or edge in visited):
+            # 리스트에서 삭제하고 넘어감
             vertexList.pop(0)
             continue
 
+        # 위 조건을 모두 충족하지 않을 경우
+
+        # 해당 간선 및 정점들 저장
         visited.append(first)
         visited.append(edge)
+        edgeList.append((first, edge))
 
+        # 리스트에서 삭제함
         vertexList.pop(0)
         
-    print(visited)
+    print(f"방문한 정점 순서 : {visited}")
+    print(f"연결할 간선 : {edgeList}")
 
 
 if __name__ == "__main__":
@@ -63,5 +88,5 @@ if __name__ == "__main__":
     print(E)
     print(AdjA)
 
-    Kruskal(E, AdjA)
+    Kruskal(E)
     
--- 과제/Prim.py
+++ 과제/Prim.py
@@ -4,6 +4,8 @@ import heapq
 
 V = [ i+1 for i in range(8) ]
 
+# 간선의 정보
+# 연결된 간선 + 가중치
 E = { 
      (1, 2) : random.randint(1, 10), 
      (1, 3) : random.randint(1, 10), 
@@ -19,7 +21,6 @@ E = {
        } 
 
 # AdjacencyArray
-
 AdjA = [ [] for _ in range(len(V))]
 for row, col in E:
     AdjA[row-1].append(col)
@@ -28,37 +29,52 @@ for row, col in E:
 # print(3 < math.inf)
 # print(3 < -math.inf)
 
-
+# 프림 알고리즘
+# 1. 시작 정점을 선택함
+# 2. 연결된 정점 중 가장 가중치가 작은 정점 선택
+# 3. 해당 간선으로 연결된 정점에서 다시 가중치가 작은 정점 선택
+# 4. 위 과정을 모든 정점이 연결될 때까지 반복
+# 간선 정보, 인접 배열, 시작 정점에 대한 정보 필요
 def Prim(E : dict, AdjA : list, start : int):
-    visited = []
-    q = []
+    visited = [] # 최소 값의 간선을 연결한 결과를 보여줄 리스트
+    q = [] # 힙큐로 저장할 리스트
 
-    visited.append(start)
+    visited.append(start) # 시작 지점 저장
 
-    prevCost = 11
+    prevCost = 11 # 이전의 간선 가중치를 확인할 변수, 10이 최대 가중치이기 때문에 시작은 11로 시작
 
-    totalCost = 0
+    totalCost = 0 # 총 가중치의 합을 저장할 변수
     
+    # 시작 정점의 이웃 정점에 대한 반복
     for neighbor in AdjA[start-1]:
-        cost = E[(start,neighbor)]
-        if(cost < prevCost):
-            neighborNode = neighbor
-            prevCost = cost
-  
+        cost = E[(start,neighbor)] # 이웃 정점과의 간선의 가중치를 구함
+        if(cost < prevCost): # 이웃 정점과의 가중치가 이전보다 작다면
+            neighborNode = neighbor # 최종 이웃 정점으로 지정
+            prevCost = cost # 현재 가중치를 이전 가중치로 설정하고 다음으로 넘어감
+
+    # 반복이 끝나고 나온 최종 이웃 정점은 시작 정점에서 연결할 수 있는 가장 최소의 가중치를 가진 정점임
+    # 이 정점에 대한 가중치와 정점 정보를 우선 순위 큐에 저장
     heapq.heappush(q, (prevCost, neighborNode))
 
+    # 우선 순위 큐가 빌 때까지 반복함
     while(q):
-        cost, node = heapq.heappop(q)
+        # 가중치와 정점 정보를 우선 순위 큐에서 꺼냄
+        cost, node = heapq.heappop(q) 
 
-
+        # 정점이 방문한 적이 있는 정점이면 넘어감
         if(node in visited):
             continue
-
+        
+        # 정점이 방문한 적 없다면 추가
         visited.append(node)
         totalCost += cost
         
+        # 현재 처리하고 있는 정점의 이웃 정점 확인
         for neighbor in AdjA[node-1]:
+
+            # 이웃 정점이 방문한 적이 없다면
             if neighbor not in visited:
+                # 해당 정점에 대한 가중치를 받아서 우선 순위 큐에 저장함
                 edgeCost = E[(min(node, neighbor), max(node, neighbor))]
                 heapq.heappush(q, (edgeCost, neighbor))
 
